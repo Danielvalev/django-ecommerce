8eaa3d41fc5e85a5571de175af61bfbd7f49c5e5
Danielvalev <danielvalev89@gmail.com>
2021-12-14 09:13:20 +0200

    [added] home page view and base template

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Category, Product
+
 
 # Create your views here.
+
+# This will allow categories to be accessible to every views, it need to be added to settings.py as well
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
+
+
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, 'store/home.html', {
+        'products': products,
+    })
