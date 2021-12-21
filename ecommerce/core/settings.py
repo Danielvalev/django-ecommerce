a26dd5131e3b4f7908120fcc19953cc03ae150c6
Danielvalev <danielvalev89@gmail.com>
2021-12-21 13:01:20 +0200

    [added] basket

--- ecommerce/core/settings.py
+++ ecommerce/core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store',
+    'basket',
 ]
 
 MIDDLEWARE = [
@@ -71,6 +72,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'store.context_processors.categories',
+                'basket.context_processors.basket',
             ],
         },
     },
