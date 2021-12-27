c06081683d5786c2e0a4582323949047cab2e129
Danielvalev <danielvalev89@gmail.com>
2021-12-27 18:04:03 +0200

    [updated] update basket

--- ecommerce/basket/basket.py
+++ ecommerce/basket/basket.py
@@ -23,7 +23,7 @@ class Basket():
         if product_id not in self.basket:
             self.basket[product_id] = {'price': str(product.price), 'qty': int(qty)}
 
-        self.session.modified = True  # Save the changes in the session
+        self.save()
 
     def __iter__(self):
         """
@@ -49,3 +49,28 @@ class Basket():
 
     def get_total_price(self):
         return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
+
+    def delete(self, product):
+        """
+        Delete item from session data
+        """
+        product_id = str(product)
+
+        if product_id in self.basket:
+            del self.basket[product_id]
+
+        self.save()
+
+    def update(self, product, qty):
+        """
+        Update values in session data
+        """
+        product_id = str(product)
+
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = qty
+
+        self.save()
+
+    def save(self):
+        self.session.modified = True  # Save the changes in the session
